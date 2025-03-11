94f3d428aaf9fbd21e8a4daa4a14ad387f4ea844
Dominic Savarino <136136566+domsav13@users.noreply.github.com>
2025-03-11 15:14:06 -0400

    Create shared_data.h

---
+++ shared_arduino/shared_data.h
@@ -0,0 +1,19 @@
+#ifndef SHARED_DATA_H
+#define SHARED_DATA_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define SHM_KEY 1234 // Shared memory key
+#define SHM_SIZE sizeof(shared_data_t)
+
+// Structure for shared data
+typedef struct {
+    uint16_t t_on;
+    uint16_t t_off;
+    uint16_t position;
+    float velocity;
+    bool newData;
+} shared_data_t;
+
+#endif

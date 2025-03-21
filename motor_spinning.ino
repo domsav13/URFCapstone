6958b1314741382954db54320f8094cd18229e4e
Jairo <131399441+jairolsanchez@users.noreply.github.com>
2025-03-21 14:45:10 -0400

    Update motor_spinning.ino
    motor stops/stutters at desired position, but then it continues spinning

--- motor_spinning.ino
+++ motor_spinning.ino
@@ -2,11 +2,16 @@ volatile uint16_t t_on = 0;  // Stores high-time (PWM on time)
 volatile uint16_t t_off = 0; // Stores low-time (PWM off time)
 volatile bool newData = false;
 
+volatile uint8_t recv_buf[16];
+volatile uint8_t recv_len = 0;
+volatile uint8_t err_status = 0;
+volatile uint16_t targetPosition = 800; 
+volatile uint16_t position = 0;
+volatile bool motorRun = true;
+
 void setup() 
 {
-  cli(); //ensures that interrupts don't interfere this block of code
-
-  Serial.begin(115200);
+  cli(); 
 
   DDRA = 0b00000011;  //sets both pins 22 and 23 as output pins
   PORTA = 0b00000000; //sets both pins 22 and 23 to start at low
@@ -32,19 +37,41 @@ void setup()
   TCCR5C = 0b00000000;
 
   TIMSK5 = 0b00100001;  // Enable Input Capture and Overflow Interrupts
-
+  
   sei();
 }
 
 void loop() 
 {
   encoderposition();  // Continuously update encoder position
+
+  if (motorRun && abs(position - targetPosition) <= 50) //if both motorRun=true and within 50 of desired position, stop motor
+  {
+    motorRun = false;  
+  }
+
+  
+  if (!motorRun && abs(position - targetPosition) > 150) //if both motorRun=false and within 50 of desired position, start motor
+  {
+    motorRun = true;  
+  }
+  
+  if (motorRun) //if motor = true, then turn timer and timer interupt on (run motor)
+  {
+    TCCR1B |= ( (1 << 1) | (1 << 3) ); 
+    TIMSK1 |= (1 << OCIE1A);
+  }
+  else //if motor = false, then turn timer and timer interupt of (stop motor)
+  {
+    TCCR1B &= ~( (1 << 1) | (1 << 3) ); 
+    TIMSK1 &= ~(1 << OCIE1A); 
+  }
 }
 
 //Timer Interrupt to set the stepper motor signal at a specified interval 
 ISR(TIMER1_COMPA_vect) 
 {
-  PINA = 0b00000011; // Toggle PA0 (Pin 22) and PA1 (Pin 23) between H/L
+  PINA = 0b00000011; // Toggle PA0 (Pin 22) and PA1 (Pin 23), turn motor on
 }
 
 //Timer interrupt to trigger once a falling or rising edge is detected 
@@ -80,71 +107,11 @@ void encoderposition()
     newData = false;
     sei();  // Re-enable interrupts (equivalent to interrupts())
 
-    // Convert counts to microseconds (each count = 0.5 µs)
     float t_on_us = highTime * 0.5;
     float t_off_us = lowTime * 0.5;
 
-    // Compute absolute position
     float x = ((t_on_us * 1026) / (t_on_us + t_off_us)) - 1;
-    uint16_t position = (x <= 1022) ? x : 1023;
 
-    //Print results
-    Serial.print("Pulse Width High: ");
-    Serial.print(t_on_us);
-    Serial.print(" µs, Low: ");
-    Serial.print(t_off_us);
-    Serial.print(" µs, Position: ");
-    Serial.println(position);
+    position = (x <= 1022) ? x : 1023;
   }
 }
-
-//pseudocode
-//define step_position, time_on, time_off (measuring time in microseconds)
-
-//detect rising edge
-//start timer for time_on
-//detect falling edge
-//stop time for time_on and save that value, start time for time_off
-//detect rising edge
-//stop time_off timer and save that value and start time_on timer again 
-
-//convert this information to position and velocity to send to rpi as packet 
-// static float velocity = 0;
-// static int16_t position = count;
-// float alpha = f_sample / (f_sample + f_corner);
-//velocity = velocity * alpha + alpha * (count - position);
-  //send both through the serial monitor and i2c
-
-  /*step_position = ((time_on * 1026) / (time_on + time_off)) - 1
-
-  if (step_position == 1024)
-  {
-    step_position = 1023;
-  }
-  else
-  {
-    step_position = step_position;
-  }
-*/
-
-/*pseudocode
-read and update speed and direction (Period) info from rpi
-Motor 1
-Port = Hi
-Wait(period/2)
-Port = LOW
-wait(period/2)
-
-read and update speed and direction (Period) info from rpi
-Motor 2
-Port = Hi
-Wait(period/2)
-Port = LOW
-wait(period/2)
-
-send imu data to rpi 
-
-send encoder data to rpi 
-
-*do i need to include interupts and how do I set the frequency at which each is done?
-*/
